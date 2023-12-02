fd4be83f1f960e4e5d4fe4ba2167c6ff24c6211e
mahdi msr <mahdi.msr4@gmail.com>
2023-12-02 21:41:18 +0330

    update candle time frame enum

--- app/Enums/CandleTimeframeEnum.php
+++ app/Enums/CandleTimeframeEnum.php
@@ -4,5 +4,16 @@ namespace App\Enums;
 
 enum CandleTimeframeEnum: string
 {
-    case Daily = '1D';
+    case EVERY_MINUTE = '1m';
+    case EVERY_FIVE_MINUTES = '5m';
+    case EVERY_FIFTEEN_MINUTES = '15m';
+    case EVERY_THIRTY_MINUTES = '30m';
+    case EVERY_HOUR = '1H';
+    case EVERY_THREE_HOURS = '3H';
+    case EVERY_FOUR_HOURS = '4H';
+    case EVERY_SIX_HOURS = '6H';
+    case EVERY_TWELVE_HOURS = '12H';
+    case DAILY = '1D';
+    case TWO_DAYS = '2D';
+    case THREE_DAYS = '3D';
 }
