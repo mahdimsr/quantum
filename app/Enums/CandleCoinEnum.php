29b00855bdf16ae62e8046a4a9b2646cba57da8d
mohammad mahdi mansouri <m.mansouri@dadehpardaz.com>
2023-12-18 12:50:52 +0330

    add symbol

--- app/Enums/CandleCoinEnum.php
+++ app/Enums/CandleCoinEnum.php
@@ -5,4 +5,14 @@ namespace App\Enums;
 enum CandleCoinEnum: int
 {
     case BTC = 1;
+
+    public function IRTSymbol(): string
+    {
+        return $this->name . 'IRT';
+    }
+
+    public function USDTSymbol(): string
+    {
+        return $this->name . 'USDT';
+    }
 }
