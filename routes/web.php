05e0c1e5f939d6d52288b180478d1664f7d72f6d
mahdi msr <mahdi.msr4@gmail.com>
2023-12-22 15:00:31 +0330

    testing calculate RSI

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    $now = now()->timestamp;
-    $twoDaysAgo = now()->subDays(2)->timestamp;
+    /**
+    0 => 1703242800
+    1 => "43733.30" open
+    2 => "43685.23" close
+    3 => "43733.30" high
+    4 => "43661.52" low
+    5 => "6.2806" volume
+    6 => "274414.217527"
+     */
 
-    $response = \App\Services\Exchange\Facade\Exchange::ohlc('BTCUSDT',ExchangeResolutionEnum::EVERY_HOUR,$now,$twoDaysAgo,100);
+    $now = now()->startOfMinute()->timestamp;
+    $twoDaysAgo = now()->startOfMinute()->subHours(6)->timestamp;
 
-    dd($response->ohlc(0));
+    $dateTest = \Illuminate\Support\Carbon::createFromTimestamp('1703242800')->toDateTimeString();
+
+    $response = \App\Services\Exchange\Facade\Exchange::ohlc('BTCUSDT',ExchangeResolutionEnum::EVERY_FIVE_MINUTES,$now,$twoDaysAgo,100);
+
+    $data = [];
+
+    for ($i = 0; $i < $response->count();$i++){
+        $candle = $response->ohlc($i);
+
+        $data[] = ['close' => $candle->close()];
+    }
+
+    $rsi = \App\Services\Indicator\Facade\Indicator::RSI($data,14);
+
+    dd($rsi);
 
     return view('welcome');
 });
