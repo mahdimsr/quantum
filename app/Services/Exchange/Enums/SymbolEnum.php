977e58442cf4cf6e5115ca7ccf643174b01f337d
mohammad mahdi mansouri <m.mansouri@dadehpardaz.com>
2023-12-24 14:43:50 +0330

    add some tokens

--- app/Services/Exchange/Enums/SymbolEnum.php
+++ app/Services/Exchange/Enums/SymbolEnum.php
@@ -6,6 +6,10 @@ enum SymbolEnum : string
 {
     case BTC = 'BTC';
     case ETH = 'ETH';
+    case FTM = 'FTM';
+    case KSM = 'KSM';
+    case SCRT = 'SCRT';
+    case MINA = 'MINA';
 
     public function toUSDT(): string
     {
