83d47edba0bffc722e40275f1f3c4e0cb14175c1
mahdi msr <mahdi.msr4@gmail.com>
2024-01-05 10:53:03 +0330

    calculate averageTrueRange

--- app/Services/Indicator/Facade/Indicator.php
+++ app/Services/Indicator/Facade/Indicator.php
@@ -16,4 +16,27 @@ class Indicator extends Facade
     {
         return IndicatorService::class;
     }
+
+    public static function averageTrueRange(array $high, array $low, array $close, int $length = 14): array
+    {
+        $trueRange = [];
+        $averageTrueRange = [];
+
+        for ($i = 1; $i < count($close); $i++) {
+            $trueRange[] = max(
+                $high[$i] - $low[$i],
+                abs($high[$i] - $close[$i - 1]),
+                abs($low[$i] - $close[$i - 1])
+            );
+        }
+
+        $averageTrueRange[0] = array_sum(array_slice($trueRange, 0, $length)) / $length;
+
+        for ($i = $length; $i < count($trueRange); $i++) {
+            $averageTrueRange[$i] = ($averageTrueRange[$i - 1] * ($length - 1) + $trueRange[$i]) / $length;
+        }
+
+        return $averageTrueRange;
+    }
+
 }
