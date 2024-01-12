01c2d988f394313a35c0faf76c612403fe751c0f
mahdi msr <mahdi.msr4@gmail.com>
2024-01-12 14:10:17 +0330

    change sma array index

--- app/Services/Indicator/Technical/SMA.php
+++ app/Services/Indicator/Technical/SMA.php
@@ -10,7 +10,7 @@ class SMA extends IndicatorStructure
 
         $sma = [];
         for ($i = $this->period - 1; $i < count($closeDataArray); $i++) {
-            $sma[$i] = array_sum(array_slice($closeDataArray, $i - $this->period + 1, $this->period)) / $this->period;
+            $sma[] = array_sum(array_slice($closeDataArray, $i - $this->period + 1, $this->period)) / $this->period;
         }
 
         return $sma;
