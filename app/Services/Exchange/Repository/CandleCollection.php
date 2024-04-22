e616fc265b06d3c2a8582f6eae3f654519629ffa
mohammad mahdi mansouri <m.mansouri@dadehpardaz.com>
2024-04-22 10:44:29 +0330

    add some function

--- app/Services/Exchange/Repository/CandleCollection.php
+++ app/Services/Exchange/Repository/CandleCollection.php
@@ -6,5 +6,28 @@ use Illuminate\Support\Collection;
 
 class CandleCollection extends Collection
 {
+    public function highs(): Collection
+    {
+        return $this->map(fn(Candle $candle) => $candle->getHigh());
+    }
 
+    public function maxHigh(): mixed
+    {
+        return $this->highs()->max();
+    }
+
+    public function lows(): Collection
+    {
+        return $this->map(fn(Candle $candle) => $candle->getLow());
+    }
+
+    public function minLow(): mixed
+    {
+        return $this->lows()->min();
+    }
+
+    public function closes(): Collection
+    {
+        return $this->map(fn(Candle $candle) => $candle->getClose());
+    }
 }
