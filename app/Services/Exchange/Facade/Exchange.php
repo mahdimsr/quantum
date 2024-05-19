0159cd05cc711b4a62053d977aa989daabae409e
mohammad mahdi mansouri <m.mansouri@dadehpardaz.com>
2024-05-19 10:59:30 +0330

    FIX place order return type

--- app/Services/Exchange/Facade/Exchange.php
+++ app/Services/Exchange/Facade/Exchange.php
@@ -7,6 +7,7 @@ use App\Services\Exchange\Coinex\Responses\OrderResponseAdapter;
 use App\Services\Exchange\Enums\ExchangeResolutionEnum;
 use App\Services\Exchange\Enums\OrderExecutionEnum;
 use App\Services\Exchange\Enums\OrderTypeEnum;
+use App\Services\Exchange\Repository\Order;
 use App\Services\Exchange\Repository\PositionLevelCollection;
 use App\Services\Exchange\Responses\AdjustPositionLeverageContract;
 use App\Services\Exchange\Responses\AdjustPositionMarginResponseContract;
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static CandleResponseContract market(string $symbol, string $period, string $limit = null)
  * @method static OrderResponseAdapter orders(string $marketType)
- * @method static mixed placeOrder(string $symbol, string $marketType, string $side, string $type, float $amount, float $price)
+ * @method static Order placeOrder(string $symbol, string $marketType, string $side, string $type, float $amount, float $price)
  * @method static AdjustPositionLeverageContract adjustPositionLeverage(string $symbol, string $marketType, string $marginMode, int $leverage)
  * @method static AdjustPositionMarginResponseContract adjustPositionMargin(string $symbol, string $marketType, string $amount)
  * @method static ClosePositionResponseContract closePosition(string $symbol, string $marketType, string $type, int $price, int $amount, ?string $customId = null)
