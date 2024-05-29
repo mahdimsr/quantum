9a56be80380682637a3552e2d6edb863f37b684c
m.abdi <m.abdi@dadehpardaz.com>
2024-05-29 16:00:42 +0330

    Add command to check sl and tp

---
+++ app/Console/Commands/CheckOrderCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use App\Services\Exchange\Facade\Exchange;
+use Illuminate\Console\Command;
+
+class CheckOrderCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'orders:check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check orders with has_stop_loss and has_take_profit';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $orders = Order::where('has_stop_loss', false)
+            ->where('has_take_profit', false)
+            ->get();
+
+        foreach ($orders as $order) {
+
+            $slResponse = Exchange::setStopLoss($order->market, 'futures', 'mark_price', $order->stop_loss_price);
+
+            if($slResponse['message'] == 'ok') {
+                $order->update(['has_stop_loss' => true]);
+            }
+            sleep(1);
+
+           $tpResponse = Exchange::setTakeProfit($order->market, 'futures', 'mark_price', $order->take_profit_price);
+            if($tpResponse['message'] == 'ok') {
+                $order->update(['has_take_profit' => true]);
+            }
+        }
+    }
+}
