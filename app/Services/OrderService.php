4a6cd30970846e836d12795c9258482b48e14beb
m.abdi <m.abdi@dadehpardaz.com>
2024-05-29 16:01:48 +0330

    Store order placed

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -23,13 +23,13 @@ class OrderService
 
         $order = Exchange::placeOrder($symbol, 'futures', $side, 'market', $amount, $currentPrice);
 
-        sleep(1);
+        $futuresOrderPrices = [
+            'current_price' => $currentPrice,
+            'stop_loss_price' => $stopLoss,
+            'take_profit_price' => $takeProfit
+        ];
 
-        Exchange::setStopLoss($symbol, 'futures', 'mark_price', $stopLoss);
-
-        sleep(1);
-
-        Exchange::setTakeProfit($symbol, 'futures', 'mark_price', $takeProfit);
+        \App\Models\Order::storeOrderRecord($order, $futuresOrderPrices);
 
         return $order;
     }
