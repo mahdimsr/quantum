47b823f2ceb95d6aaf898be191564803228682bc
mahdi msr <mahdi.msr4@gmail.com>
2024-06-03 07:51:37 +0330

    implement FilamentUser to handle access panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -28,5 +29,10 @@ class User extends Authenticatable
     public static function findByEmail(string $email): Model
     {
         return self::query()->where('email',$email)->firstOrFail();
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
