d2234630e3dfe31ff367060714eb06fcc70481f6
mahdimsr <mahdi.msr4@gmail.com>
2024-10-10 15:33:53 +0330

    remove useless used class

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,11 +3,7 @@
 namespace App\Models;
 
 use App\Observers\OrderObserver;
-use App\Services\Exchange\Repository\Order as OrderRepository;
-use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Order extends Model
 {
