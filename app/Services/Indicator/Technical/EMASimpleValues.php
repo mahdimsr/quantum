831abadab44e03eda31e0bfee676e32a97276a0b
mahdimsr <mahdi.msr4@gmail.com>
2024-10-14 23:00:25 +0330

    fix calculate ema with simple values

--- app/Services/Indicator/Technical/EMASimpleValues.php
+++ app/Services/Indicator/Technical/EMASimpleValues.php
@@ -6,23 +6,30 @@ use Illuminate\Support\Collection;
 
 class EMASimpleValues
 {
-    private array $closePriceArray;
+    private array $values;
     private int $period;
 
     public function __construct(array $values, int $period = 9)
     {
-        $this->closePriceArray = $values;
+        $this->values = $values;
         $this->period = $period;
     }
 
     public function run(): array
     {
-        $multiplier = 2 / ($this->period + 1);
-        $ema = [];
-        $ema[0] = $this->closePriceArray[0];
+        $alpha = 2 / ($this->period + 1);
+        $length = count($this->values) - 1;
 
-        for ($i = 1; $i < count($this->closePriceArray); $i++) {
-            $ema[$i] = ($this->closePriceArray[$i] - $ema[$i - 1]) * $multiplier + $ema[$i - 1];
+        $ema = [];
+        $ema[$length] = $this->values[$length];
+
+        for ($i = $length - 1; $i >= 0 ; $i--) {
+
+            $preEma = $ema[$i + 1];
+
+            $exactValue = $alpha * $this->values[$i] + (1 - $alpha) * $preEma;
+
+            $ema[$i] = round($exactValue, 4);
         }
 
         return $ema;
