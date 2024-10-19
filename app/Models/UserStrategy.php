44a2f2a6e52d11e10e6a83d1024cc484a36ee3c3
mohammad mahdi mansouri <m.mansouri@dadehpardaz.com>
2024-10-19 12:37:22 +0330

    create user-strategy model & migration

---
+++ app/Models/UserStrategy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int user_id
+ * @property string name
+ * @property string balance
+ */
+class UserStrategy extends Model
+{
+
+}
