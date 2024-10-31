00883514cad442cc3229012964966c0eecbe62b8
mahdimsr <mahdi.msr4@gmail.com>
2024-10-31 12:04:19 +0330

    add close order action

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -2,12 +2,15 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\OrderStatusEnum;
+use App\Events\OrderClosedEvent;
 use App\Filament\Resources\OrderResource\Pages;
-use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\Order;
+use App\Services\Exchange\BingX\BingXService;
 use App\Services\Exchange\Enums\SideEnum;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -35,15 +38,45 @@ class OrderResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('coin_name'),
                 Tables\Columns\TextColumn::make('status')
-                ->badge(),
+                    ->badge(),
                 Tables\Columns\TextColumn::make('side')
-                ->badge(),
+                    ->badge(),
                 Tables\Columns\TextColumn::make('balance')
             ])
             ->filters([
-                //
+                Tables\Filters\Filter::make('pending')
+                    ->query(fn(Builder $query) => $query->where('status', OrderStatusEnum::PENDING->value))
+                    ->default()
             ])
             ->actions([
+                Tables\Actions\Action::make('close')
+                    ->requiresConfirmation()
+                    ->visible(fn(Order $order): bool => $order->status == OrderStatusEnum::PENDING)
+                    ->disabled(fn(Order $order): bool => $order->status != OrderStatusEnum::PENDING)
+                    ->action(function (Order $order) {
+
+                        $bingXService = app(BingXService::class);
+
+                        $closeOrderResponse = $bingXService->closePositionByPositionId($order->position_id);
+
+                        if ($closeOrderResponse->isSuccess()) {
+
+                            event(new OrderClosedEvent($order));
+
+                            Notification::make()
+                                ->title('Order Closed')
+                                ->success()
+                                ->send();
+
+                        } else {
+
+                            Notification::make()
+                                ->title('Sth Wrong ...')
+                                ->danger()
+                                ->send();
+                        }
+
+                    })
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
