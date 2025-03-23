a12f7746c32080a2c5021b656635290cabc5bf00
mahdimsr <mahdi.msr4@gmail.com>
2025-03-23 23:42:49 +0330

    log update sl command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
         })->hourlyAt(30)->appendOutputTo(storage_path('logs/commands/dynamic-reward.log'));
 
 
-        $schedule->command('app:update-dynamic-stop-loss-command')->everyThirtyMinutes();
+        $schedule->command('app:update-dynamic-stop-loss-command')->everyThirtyMinutes()->appendOutputTo(storage_path('logs/dynamic-stop-loss.log'));
     }
 
     /**
