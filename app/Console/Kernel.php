55e9bd7ec3adb126b5c13b705f761f802ab60732
mahdimsr <mahdi.msr4@gmail.com>
2025-04-21 19:24:03 +0330

    update commands and remove useless ones

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,21 +2,11 @@
 
 namespace App\Console;
 
-use App\Console\Commands\StaticRewardCommand;
-use App\Enums\CoinEnum;
-use App\Enums\CoinStatusEnum;
-use App\Enums\OrderStatusEnum;
-use App\Enums\StrategyEnum;
-use App\Enums\TimeframeEnum;
-use App\Models\Coin;
-use App\Models\Order;
-use App\Services\Exchange\Enums\ExchangeResolutionEnum;
-use App\Services\Exchange\Enums\SymbolEnum;
-use Carbon\Carbon;
+use App\Console\Commands\CloseOrbitalOrdersCommand;
+use App\Console\Commands\OrbitalStrategyCommand;
+use App\Console\Commands\UpdateOrderPositionIdCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Queue;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,20 +15,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->call(function () {
+        $schedule->command(UpdateOrderPositionIdCommand::class)->everyMinute();
 
-            $coins = Coin::withStrategies(StrategyEnum::DYNAMIC_REWARD)->get();
+        $schedule->command(OrbitalStrategyCommand::class)->everyThirtyMinutes();
+        $schedule->command(CloseOrbitalOrdersCommand::class)->everyThirtyMinutes();
 
-            foreach ($coins as $coin) {
-
-                Artisan::call('app:dynamic-reward-strategy', ['--coin' => $coin->name]);
-            }
-
-        })->hourlyAt(30)->appendOutputTo(storage_path('logs/commands/dynamic-reward.log'));
-
-
-        $schedule->command('app:update-dynamic-stop-loss-command')->everyThirtyMinutes()->appendOutputTo(storage_path('logs/dynamic-stop-loss.log'));
-        $schedule->command('app:close-position')->hourlyAt(15)->appendOutputTo(storage_path('logs/close-position.log'));
     }
 
     /**
