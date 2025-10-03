26e47df65c75e5764d86dad1d85da216ff44631e
mahdi msr <mahdi.msr4@gmail.com>
2025-10-03 13:09:21 +0330

    bitunix: add setLeverage request

--- app/Services/Exchange/Bitunix/BitunixService.php
+++ app/Services/Exchange/Bitunix/BitunixService.php
@@ -4,13 +4,17 @@ namespace App\Services\Exchange\Bitunix;
 
 use App\Services\Exchange\Bitunix\Responses\AssetBalanceResponseAdapter;
 use App\Services\Exchange\Bitunix\Responses\CandleResponseAdapter;
+use App\Services\Exchange\Bitunix\Responses\SetLeverageResponseAdapter;
+use App\Services\Exchange\Enums\SideEnum;
 use App\Services\Exchange\Requests\AssetRequestContract;
 use App\Services\Exchange\Requests\CandleRequestContract;
+use App\Services\Exchange\Requests\SetLeverageRequestContract;
 use App\Services\Exchange\Responses\AssetBalanceContract;
 use App\Services\Exchange\Responses\CandleResponseContract;
+use App\Services\Exchange\Responses\SetLeverageResponseContract;
 use Msr\LaravelBitunixApi\Facades\LaravelBitunixApi;
 
-class BitunixService implements CandleRequestContract, AssetRequestContract
+class BitunixService implements CandleRequestContract, AssetRequestContract, SetLeverageRequestContract
 {
 
     /**
@@ -40,5 +44,19 @@ class BitunixService implements CandleRequestContract, AssetRequestContract
         $data = json_decode($response->getBody()->getContents(), true);
 
         return new AssetBalanceResponseAdapter($data);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function setLeverage(string $symbol, SideEnum $side, string $leverage): SetLeverageResponseContract
+    {
+        $response = LaravelBitunixApi::changeLeverage($symbol, 'USDT', intval($leverage));
+
+        throw_if($response->getStatusCode() != 200, 'No success response');
+
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        return new SetLeverageResponseAdapter($data);
     }
 }
